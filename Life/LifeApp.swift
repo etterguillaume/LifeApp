749ef56509189afb3a18f45b65e836f4fddb85fe
Guillaume Etter <etterguillaume@gmail.com>
2024-01-17 21:39:04 -0500

    Initial Commit

---
+++ Life/LifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeApp.swift
+//  Life
+//
+//  Created by Guillaume Etter on 2024-01-17.
+//
+
+import SwiftUI
+
+@main
+struct LifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
