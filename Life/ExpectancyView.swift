7a19dd952c1a7eeb3f0b004b29128a0b3c8f8de2
Guillaume Etter <etterguillaume@gmail.com>
2024-01-18 22:37:16 -0500

    Added logo, locked orientation

---
+++ Life/ExpectancyView.swift
@@ -0,0 +1,8 @@
+//
+//  ExpectancyView.swift
+//  Life
+//
+//  Created by Guillaume Etter on 2024-01-18.
+//
+
+import Foundation
---
+++ Life/UserInputView.swift
@@ -0,0 +1,8 @@
+//
+//  UserInputView.swift
+//  Life
+//
+//  Created by Guillaume Etter on 2024-01-18.
+//
+
+import Foundation
